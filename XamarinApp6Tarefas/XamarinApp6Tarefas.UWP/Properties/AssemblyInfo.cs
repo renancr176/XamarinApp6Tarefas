0dfb252c807518c650392cd983039a866db03de0
RENAN DE CARVALHO RIBEIRO <renancr176@gmail.com>
2019-01-30 16:02:42 -0200

    Versão 1.2 Android e 1.0 UWP
    Adicionado icone e titulo do aplicativo

--- XamarinApp6Tarefas/XamarinApp6Tarefas.UWP/Properties/AssemblyInfo.cs
+++ XamarinApp6Tarefas/XamarinApp6Tarefas.UWP/Properties/AssemblyInfo.cs
@@ -1,11 +1,12 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XamarinApp6Tarefas.UWP")]
+[assembly: AssemblyTitle("Tarefas")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -27,3 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("pt-BR")]
+
